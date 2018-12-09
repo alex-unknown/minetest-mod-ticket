7b8ca253b2818e9db2762e550c676cb3be2bf4da
unknown <40572718+alex-unknown@users.noreply.github.com>
2018-12-09 12:18:50 +0100

    removed id; added mod_storage

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 --[[
-	dice Mod [dice3]
-	================
+	ticket Mod [ticket]
+	===================
 	v0.1 by unknown
 	Copyright (C) 2017-2018 Alexander K.
 	idea: maxx :)
@@ -10,12 +10,40 @@
 
 
 
+local mod_storage = minetest.get_mod_storage()
+
+local function del_old_players()
+	mod_storage:set_string("d","d")
+	local num = 0
+	local t = mod_storage:to_table()
+	local newt ={fields = {}}
+	for description,playerS in pairs(t.fields) do
+		if minetest.player_exists(playerS) then
+			newt.fields[description] = playerS
+		else 
+			num = num + 1
+		end
+	end
+	mod_storage:from_table(newt)
+	return num
+end
+
+minetest.register_chatcommand("ticket_remove_old_players", {
+	description = "Delete tickets of old players",
+	privs = {debug = true},
+	func = function(_,_)
+		local num = del_old_players()
+		return true,"Successfully removed "..num.." player/s!"
+	end,
+})
+
+del_old_players()
+
 local function show_formspec(player)
 	minetest.show_formspec(player:get_player_name(), "ticket:write_formspec",
-                "size[4.5,3.5]" ..
+                "size[4.5,2.5]" ..
                 "field[1,1;3,1;description;Description;]" ..
-				"field[1,2;3,1;id;ID (just enter anything);]" ..
-                "button_exit[1.25,2.5;2,1;exit;Save]")
+                "button_exit[1.25,1.5;2,1;exit;Save]")
 end
 
 minetest.register_craftitem("ticket:unlabeled_ticket", {
@@ -42,15 +70,20 @@ minetest.register_craftitem("ticket:ticket", {
 minetest.register_on_player_receive_fields(function(player,formname, fields)
 	local playerS = player:get_player_name()
     if formname ~= "ticket:write_formspec" then return false end
-	if not fields.id or fields.id == "" or not fields.description or fields.description == "" then 
-		minetest.chat_send_player(playerS,"[ticket] Please enter description and id!")
+	if not fields.description or fields.description == "" then 
+		minetest.chat_send_player(playerS,"[ticket] Please enter description!")
 		return false
 	end
+	if mod_storage:get_string("do_"..fields.description) ~= "" and
+	   mod_storage:get_string("do_"..fields.description) ~= playerS then 
+		minetest.chat_send_player(playerS,"[ticket] This description is already taken!")
+		return false
+	end
+	mod_storage:set_string("do_"..fields.description, playerS)
 	local itemstack = player:get_wielded_item()
 	if not itemstack or itemstack:is_empty() then return false end
 	itemstack:set_name("ticket:ticket")
 	local meta = itemstack:get_meta()
-	meta:set_string("", fields.id)
 	meta:set_string("description", fields.description)
 	player:set_wielded_item(itemstack)
 	minetest.chat_send_player(playerS,"[ticket] Successfully created ticket!")
